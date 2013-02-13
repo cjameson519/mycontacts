4d19b7e3bd1979d4fb36e618f33e73cad9c1cbab
unknown <cjameson519@wit-xdvm-054.MILLARDPS.NET>
2013-02-13 13:35:54 -0600

    made some changes in css and added add_contacts

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+session_start();
+
+require('config/db.php');
+require('config/app.php');
+require('lib/functions.php');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+	</head>
+	<body>
+		<div id="wrapper" class="container">
+			<header>
+				<?php include('layout/header.php') ?>
+			</header>
+			<nav>
+				<?php include('layout/nav.php') ?>
+			</nav>
+			<section role="main">
+				<?php include('layout/main.php') ?>
+			</section>
+			<footer>
+				<?php include('layout/footer.php') ?>
+			</footer>
+		</div>
+	</body>
+</html>
